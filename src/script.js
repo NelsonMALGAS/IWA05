91db50d3d71e35d39a5b3ef2aa5d9dff1840a329
NelsonMALGAS <zmalgas69@gmail.com>
2023-04-01 17:58:36 +0200

    first commit

---
+++ src/script.js
@@ -0,0 +1,59 @@
+const FREE_WARNING = 'Free shipping only applies to single customer orders';
+const BANNED_WARNING = 'Unfortunately we do not ship to your country of residence';
+const NONE_SELECTED = '0';
+const customers = '1';
+const custlocation = 'RSA';
+let locationA='NAM'
+let currency = '$';
+let shipping;  //declare shipping without assigning a value yet
+
+// Use triple equals(===) instead of = for strict string camparison 
+if (custlocation == 'RSA') {
+     shipping = '400'; 
+     currency == 'R'; // Updae currency for RSA custlocation
+ }else if (locationA == 'NAM'){
+    shipping = '600' ;
+    currency ='$';
+ } else{
+    shipping='800';
+ }
+    console.log(FREE_WARNING);// handle free warning locations
+ 
+ 
+// Use correct variable name( toys = instead of toys-)
+const shoes = 300 * 1;
+const toys = 100 * 5;
+let shirts = 150 * NONE_SELECTED ;// Use let instead of const to allow re-assignment
+const batteries = 35 * 2;
+let pens = 5 * NONE_SELECTED;// Use let instead of const to allow re-assignment 
+
+
+if (shoes + batteries + pens + shirts + toys > 1000 && currency==='$' ) {
+	if (locationA ==' NAM' && customers < 2) {
+        shipping='800'; //update shipping for NAM location and less than 2 customres
+			if (custlocation == 'RSA'){
+		    shipping = 0 || calcShipping();//use correct function name
+		}
+	}
+}
+
+
+if (shipping == '400' && customers !==1) {
+    (currency =='R') 
+    console.log(shipping='0') ;
+}
+
+if (custlocation == 'NK') {
+    console.log(BANNED_WARNING); 
+   
+}else{
+    (currency =='R')
+    console.log('price','R', shoes + batteries + pens + shirts + toys + shipping);
+    
+}
+function calcShipping(){
+    return 0;
+}
+
+
+
