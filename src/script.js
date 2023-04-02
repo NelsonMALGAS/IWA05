4ad78b3ab9f702adb7abd2948cf383932fd5e98d
NelsonMALGAS <zmalgas69@gmail.com>
2023-04-02 17:30:18 +0200

    modified script.js

--- src/script.js
+++ src/script.js
@@ -8,10 +8,10 @@ let currency = '$';
 let shipping;  //declare shipping without assigning a value yet
 
 // Use triple equals(===) instead of = for strict string camparison 
-if (custlocation == 'RSA') {
+if (custlocation === 'RSA') {
      shipping = '400'; 
-     currency == 'R'; // Updae currency for RSA custlocation
- }else if (locationA == 'NAM'){
+     currency === 'R'; // Update currency for RSA custlocation
+ }else if (locationA === 'NAM'){
     shipping = '600' ;
     currency ='$';
  } else{
@@ -28,31 +28,31 @@ const batteries = 35 * 2;
 let pens = 5 * NONE_SELECTED;// Use let instead of const to allow re-assignment 
 
 
-if (shoes + batteries + pens + shirts + toys > 1000 && currency==='$' ) {
-	if (locationA ==' NAM' && customers < 2) {
-        shipping='800'; //update shipping for NAM location and less than 2 customres
-			if (custlocation == 'RSA'){
-		    shipping = 0 || calcShipping();//use correct function name
+if (shoes + batteries + pens + shirts + toys > 1000 && currency ==='$' ) {
+	if (locationA ===' NAM' && customers < 2) {
+        shipping='600'; //update shipping for NAM location and less than 2 customres
+			if (custlocation === 'RSA'){
+		    shipping = '0' || calcShipping();//use of correct function name
 		}
 	}
 }
 
 
-if (shipping == '400' && customers !==1) {
-    (currency =='R') 
-    console.log(shipping='0') ;
+if (shipping === '400' && customers !==1) {
+    (currency ==='R') 
+    console.log(shipping = 0) ;
 }
 
-if (custlocation == 'NK') {
-    console.log(BANNED_WARNING); 
+if (custlocation === 'NK') {
+    console.log(BANNED_WARNING);// Handle invalid location 
    
 }else{
-    (currency =='R')
+    (currency ==='R')
     console.log('price','R', shoes + batteries + pens + shirts + toys + shipping);
     
 }
 function calcShipping(){
-    return 0;
+    return 0; //Impliment logic to calculate shipping
 }
 
 
